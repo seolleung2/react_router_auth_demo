ae80456737d3e31ea3a35f90c385d4ad75c6c91a
seolleung2 <seolleung22@gmail.com>
2024-10-10 17:42:28 +0900

    add Protected Route component on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home";
 import LoginPage from "./pages/Login";
+import { AuthProvider } from "./hooks/useAuth";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import ProfilePage from "./pages/Profile";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </AuthProvider>
   );
 }
 
